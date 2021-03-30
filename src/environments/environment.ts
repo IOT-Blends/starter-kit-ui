66114df1c9216b6e77a2ab266d3c8f7cc7346bc1
Gowtham Venugopalan <gowtham.venugopalan@cognizant.com>
2021-03-30 14:58:18 +0530

    Updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCLvZ3zmqYLuLBls7utCfZHXcD5NsJeCtc",
-    authDomain: "gfit-fd0ab.firebaseapp.com",
-    databaseURL: "https://gfit-fd0ab-default-rtdb.firebaseio.com",
-    projectId: "gfit-fd0ab",
-    storageBucket: "gfit-fd0ab.appspot.com",
-    messagingSenderId: "321705514111",
-    appId: "1:321705514111:web:f7411acc644334f201db4a"
+    
   }
 };
 
